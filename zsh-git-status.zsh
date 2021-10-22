abb6dd6f306bcf80a7b9a46bd24b4cb9bf708bf5
Jonathan Clerck <j.m.clerck@icloud.com>
2021-10-22 17:04:26 +0100

    Replace ripgrep

--- zsh-git-status.zsh
+++ zsh-git-status.zsh
@@ -11,49 +11,49 @@ function git_status_main() {
   if [[ $git == true ]]; then
     local branch=$(git -C $1 rev-parse --abbrev-ref HEAD)
 
-    local stashes=$(git -C $1 stash list | rg -o '@' |  tr -d ' ' | tr -d '\n')
+    local stashes=$(git -C $1 stash list | grep -o '@' |  tr -d ' ' | tr -d '\n')
     local numberOfStashes=${#stashes}
     if [[ $numberOfStashes -gt 0 ]]; then
         icons+=("%{$fg[magenta]%} $numberOfStashes")
     fi
 
-    local untracked=$(git -C $1 status --porcelain | rg -o '^\?\?\s' |  tr -d ' ' | tr -d '\n')
+    local untracked=$(git -C $1 status --porcelain | grep -o '^\?\?\s' |  tr -d ' ' | tr -d '\n')
     local numberOfUntracked=${#untracked}
     if [[ $numberOfUntracked -gt 0 ]]; then
         icons+=("%{$fg[yellow]%} $(($numberOfUntracked / 2))")
     fi
 
-    local added=$(git -C $1 status --porcelain | rg -o '^\sA\s|^A\s{2}' |  tr -d ' ' | tr -d '\n')
+    local added=$(git -C $1 status --porcelain | grep -o '^\sA\s|^A\s{2}' |  tr -d ' ' | tr -d '\n')
     local numberOfAdded=${#added}
     if [[ $numberOfAdded -gt 0 ]]; then
         icons+=("%{$fg[green]%} $numberOfAdded")
     fi
 
-    local deleted=$(git -C $1 status --porcelain | rg -o '^\sD\s|^D\s{2}' |  tr -d ' ' | tr -d '\n')
+    local deleted=$(git -C $1 status --porcelain | grep -o '^\sD\s|^D\s{2}' |  tr -d ' ' | tr -d '\n')
     local numberOfDeleted=${#deleted}
     if [[ $numberOfDeleted -gt 0 ]]; then
         icons+=("%{$fg[red]%} $numberOfDeleted")
     fi
 
-    local modified=$(git -C $1 status --porcelain | rg -o '^\sM\s|^M\s{2}' |  tr -d ' ' | tr -d '\n')
+    local modified=$(git -C $1 status --porcelain | grep -o '^\sM\s|^M\s{2}' |  tr -d ' ' | tr -d '\n')
     local numberOfModified=${#modified}
     if [[ $numberOfModified -gt 0 ]]; then
         icons+=("%{$fg[yellow]%} $numberOfModified")
     fi
 
-    local renamed=$(git -C $1 status --porcelain | rg -o '^\sR\s|^R\s{2}' |  tr -d ' ' | tr -d '\n')
+    local renamed=$(git -C $1 status --porcelain | grep -o '^\sR\s|^R\s{2}' |  tr -d ' ' | tr -d '\n')
     local numberOfRenamed=${#renamed}
     if [[ $numberOfRenamed -gt 0 ]]; then
         icons+=("%{$fg[green]%} $numberOfRenamed")
     fi
 
-    local conflicts=$(git -C $1 status --porcelain | rg -o '^UU\s' |  tr -d ' ' | tr -d '\n')
+    local conflicts=$(git -C $1 status --porcelain | grep -o '^UU\s' |  tr -d ' ' | tr -d '\n')
     local numberOfConflicts=${#conflicts}
     if [[ $numberOfConflicts -gt 0 ]]; then
         icons+=("%{$fg[red]%} $(($numberOfConflicts / 2))")
     fi
 
-    local staged=$(git -C $1 status --porcelain |  rg -o '^A\s{2}|^D\s{2}|^M\s{2}|^R\s{2}' | tr -d ' ' | tr -d '\n')
+    local staged=$(git -C $1 status --porcelain |  grep -o '^A\s{2}|^D\s{2}|^M\s{2}|^R\s{2}' | tr -d ' ' | tr -d '\n')
     local numberOfStaged=${#staged}
     if [[ $numberOfStaged -gt 0 ]]; then
         icons+=("%{$fg[green]%} $(($numberOfStaged))")
